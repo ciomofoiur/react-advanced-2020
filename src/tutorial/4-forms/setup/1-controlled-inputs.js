60bb50f220cb9df2033754e7e18f291ccf9763b1
Raoul Ciomofoiu <raoul.ciomofoiu@itperspectives.ro>
2021-12-06 12:50:48 +0200

    Form Basics

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,28 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const handleSubmit = (e) => {
+    e.preventDefault(); //the page refreshes on submit
+    console.log('hello world'); //thus we can't see the 'hello world'
+  };
+
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name : </label>
+            <input type='text' id='firstName' name='firstName' />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email : </label>
+            <input type='text' id='email' name='email' />
+          </div>
+          <button type='submit'>add person</button>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
