45e56c9de9e02508d2cb67ee8861b1aad7396a98
Raoul Ciomofoiu <raoul.ciomofoiu@itperspectives.ro>
2021-12-06 15:28:28 +0200

    Add item to the list

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,10 +8,19 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault(); //the page refreshes on submit by default
-    console.log(firstName, email);
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email }; //ES6 cool shorthand
+      console.log(person);
+      setPeople((people) => [...people, person]);
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('empty values');
+    }
   };
 
   return (
@@ -40,6 +49,15 @@ const ControlledInputs = () => {
           </div>
           <button type='submit'>add person</button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className='item' key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
